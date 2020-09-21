a4d5769adb82cf14a2f89d5d5951538271843a72
Yoshio Kato <yoshio.kato@pf.is.s.u-tokyo.ac.jp>
2020-09-21 23:19:21 +0900

    add more options for cutout

--- tools/custom_augmentation.py
+++ tools/custom_augmentation.py
@@ -35,7 +35,57 @@ class Cutout(Augmentation):
     Cutout.
     """
 
-    def __init__(self, prob=0.5, *, size_pct=0.1, num=10):
+    def __init__(self, prob=0.5, *, size_pct=(0.02, 0.2), aspect=(0.33, 3), num=1):
+        """
+        Args:
+            prob (float): probability between 0.0 and 1.0 that
+                the wrapper transformation is applied
+            size_pct (float or tuple[float]): the area ratio of boxes w.r.t image..
+            aspect (float or tuple[float]): aspect ratio of boxes. height/width
+            num (int or tuple[int]): the maximum number of boxes
+        """
+        super().__init__()
+        assert 0.0 <= prob <= 1.0, f"Probablity must be between 0.0 and 1.0 (given: {prob})"
+        if type(size_pct) is tuple:
+            assert 0.0 <= size_pct[0] <= 1.0, f"Pct must be between 0.0 and 1.0 (given: {size_pct})"
+            assert 0.0 <= size_pct[1] <= 1.0, f"Pct must be between 0.0 and 1.0 (given: {size_pct})"
+        else:
+            assert 0.0 <= size_pct <= 1.0, f"Pct must be between 0.0 and 1.0 (given: {size_pct})"
+        self.prob = prob
+        self.size_pct = size_pct if type(size_pct) is tuple else (size_pct, size_pct)
+        self.size_pct_scale = self.size_pct[1] - self.size_pct[0]
+        self.aspect = aspect if type(aspect) is tuple else (aspect, aspect)
+        self.aspect_scale = self.aspect[1] - self.aspect[0]
+        self.num = num if type(num) is tuple else (num, num)
+    
+    def get_transform(self, img):
+        h, w = img.shape[:2]
+        area = h * w
+        do = self._rand_range() < self.prob
+        if do:
+            num = np.random.randint(self.num[0], self.num[1]+1)
+            rects = []
+            for _ in range(num):
+                _size_pct = np.random.rand() * self.size_pct_scale + self.size_pct[0]
+                _area = area * _size_pct
+                _aspect = np.random.rand() * self.aspect_scale + self.aspect[0]
+                _h = int(np.sqrt(_area * _aspect))
+                _w = int(np.sqrt(_area / _aspect))
+
+                x0 = np.random.randint(0, w)
+                x1 = min(x0 + _w, w)
+                y0 = np.random.randint(0, h)
+                y1 = min(y0 + _h, h)
+                rects.append((x0, y0, x1, y1))
+            return CutoutTransform(rects)
+        else:
+            return NoOpTransform()
+    
+class ObjectAwareCutout(Augmentation):
+    """
+    Region aware cutout.
+    """
+    def __init__(self, prob=0.5, *, size_pct=0.1, num=5):
         """
         Args:
             prob (float): probability between 0.0 and 1.0 that
@@ -49,23 +99,7 @@ class Cutout(Augmentation):
         self.prob = prob
         self.size_pct = size_pct
         self.num = num
-    
+        
     def get_transform(self, img):
-        h, w = img.shape[:2]
-        do = self._rand_range() < self.prob
-        if do:
-            num = np.random.randint(0, self.num+1)
-            length = int(self.size_pct * min(h, w))
-            rects = []
-            for _ in range(num):
-                x0 = np.random.randint(0, w)
-                x1 = min(x0 + length, w)
-                y0 = np.random.randint(0, h)
-                y1 = min(y0 + length, h)
-                rects.append((x0, y0, x1, y1))
-            return CutoutTransform(rects)
-        else:
-            return NoOpTransform()
-    
-            
+        raise NotImplementedError
 
